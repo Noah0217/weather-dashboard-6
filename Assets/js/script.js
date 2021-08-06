015e174729d9f3b69be6a62e1e53c48af5fba6ff
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-06 00:39:40 -0600

    Fixed missing curly bracket

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -24,28 +24,28 @@ function loadPage() {
            console.log(response);
 
            //Displays Weather Conditions 
-           var currentDate = new Date(response.data.dt*1000);
-           console.log(currentDate);
-           var day = currentDate.getDate();
-           var month = currentDate.getMonth() + 1;
-           var year = currentDate.getFullYear();
-           nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
-           var weatherPic = response.data.weather[0].icon;
-           currentPicEl.setAttribute("src","https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
-           currentPicEl.setAttribute("alt",response.data.weather[0].description);
-           currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
-           currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
-           currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
-           var lat = response.data.coord.lat;
-           var lon = response.data.coord.lon;
-           var UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
-           axios.get(UVQueryURL)
-           .then(function(response){
-           var UVIndex = document.createElement("span");
-           UVIndex.setAttribute("class","badge badge-danger");
-           UVIndex.innerHTML = response.data[0].value;
-           currentUVEl.innerHTML = "UV Index: ";
-           currentUVEl.append(UVIndex);
+     var currentDate = new Date(response.data.dt*1000);
+     console.log(currentDate);
+     var day = currentDate.getDate();
+       var month = currentDate.getMonth() + 1;
+      var year = currentDate.getFullYear();
+     nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+      var weatherPic = response.data.weather[0].icon;
+     currentPicEl.setAttribute("src","https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+      currentPicEl.setAttribute("alt",response.data.weather[0].description);
+      currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+     currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+        currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+        var lat = response.data.coord.lat;
+     var lon = response.data.coord.lon;
+       var UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
+       axios.get(UVQueryURL)
+         .then(function(response){
+         var UVIndex = document.createElement("span");
+        UVIndex.setAttribute("class","badge badge-danger");
+       UVIndex.innerHTML = response.data[0].value;
+         currentUVEl.innerHTML = "UV Index: ";
+        currentUVEl.append(UVIndex);
        });
 
        //API Link For Weather Forecast
@@ -57,17 +57,17 @@ function loadPage() {
            const forecastEls = document.querySelectorAll(".forecast");
            for (i=0; i<forecastEls.length; i++) {
                forecastEls[i].innerHTML = "";
-               var forecastIndex = i*8 + 4;
-               var forecastDate = new Date(response.data.list[forecastIndex].dt * 1000);
-               var forecastDay = forecastDate.getDate();
-               var forecastMonth = forecastDate.getMonth() + 1;
-               var forecastYear = forecastDate.getFullYear();
-               var forecastDateEl = document.createElement("p");
-               forecastDateEl.setAttribute("class","mt-3 mb-0 forecast-date");
-               forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
-               forecastEls[i].append(forecastDateEl);
-               var forecastWeatherEl = document.createElement("img");
-               forecastWeatherEl.setAttribute("src","https://openweathermap.org/img/wn/" + response.data.list[forecastIndex].weather[0].icon + "@2x.png");
+        var forecastIndex = i*8 + 4;
+        var forecastDate = new Date(response.data.list[forecastIndex].dt * 1000);
+        var forecastDay = forecastDate.getDate();
+        var forecastMonth = forecastDate.getMonth() + 1;
+        var forecastYear = forecastDate.getFullYear();
+        var forecastDateEl = document.createElement("p");
+       forecastDateEl.setAttribute("class","mt-3 mb-0 forecast-date");
+         forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+         forecastEls[i].append(forecastDateEl);
+         var forecastWeatherEl = document.createElement("img");
+         forecastWeatherEl.setAttribute("src","https://openweathermap.org/img/wn/" + response.data.list[forecastIndex].weather[0].icon + "@2x.png");
                forecastWeatherEl.setAttribute("alt",response.data.list[forecastIndex].weather[0].description);
                forecastEls[i].append(forecastWeatherEl);
                var forecastTempEl = document.createElement("p");
@@ -98,12 +98,12 @@ function loadPage() {
    function renderSearchHistory() {
        historyEl.innerHTML = "";
        for (let i=0; i<searchHistory.length; i++) {
-           var historyItem = document.createElement("input");
-           historyItem.setAttribute("type","text");
-           historyItem.setAttribute("readonly",true);
-           historyItem.setAttribute("class", "form-control d-block bg-white");
-           historyItem.setAttribute("value", searchHistory[i]);
-           historyItem.addEventListener("click",function() {
+     var historyItem = document.createElement("input");
+     historyItem.setAttribute("type","text");
+     historyItem.setAttribute("readonly",true);
+     historyItem.setAttribute("class", "form-control d-block bg-white");
+     historyItem.setAttribute("value", searchHistory[i]);
+     historyItem.addEventListener("click",function() {
                getWeather(historyItem.value);
            })
            historyEl.append(historyItem);
